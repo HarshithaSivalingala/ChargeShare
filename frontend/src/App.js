76b8b420db09ae8f22219d0f3f1f8de339b2093c
Sai Harshitha Sivalingala <saiharshithasivalingala@Mac.home.local>
2025-02-15 23:14:20 -0600

    Created Different Components of Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Routes} from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import HomeScreen from "./screens/HomeScreen";
+import StationScreen from "./screens/StationScreen";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+  return <Router>
+    <Routes>
+    <Route path="/login" element={<LoginScreen/>} />
+    <Route path="/profile" element={<ProfileScreen/>} />
+    <Route path="/home" element={<HomeScreen/>} />
+    <Route path="/chargestations" element={<StationScreen/>} />
+    </Routes>
+    
+  </Router>
+};
 export default App;
---
+++ frontend/src/screens/StationScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+const StationScreen = () => {
+    return <h1>Charge stations</h1>
+}
+export default StationScreen;
