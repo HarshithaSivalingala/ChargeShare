8fbaa00de66d54609dc1bf6ad74c73a4cb94618c
Sai Harshitha Sivalingala <saiharshithasivalingala@Mac.home.local>
2025-02-17 00:46:24 -0600

    added functionality in login screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,12 @@
+import React from "react";
 const HomeScreen = () => {
-    return <h1>Home</h1>
+    const str=":)";
+    return (
+
+        <div>
+            <img src="download.jpeg" alt="home"/>
+            <h1>Hi welcome to ChargeShare {str}</h1>
+        </div>
+    );
 }
 export default HomeScreen;
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,8 +1,53 @@
 import React from "react";
+import FormContainer from "../components/FormContainer";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 const LoginScreen = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault(); //browser refresh handler
+    console.log(email, password);
+    if(email === "cutu@g.com" && password === "cutuu")
+      {navigate("/home");}
+    else{
+      alert("Wrong credentials");
+    }
+  };
+
   return (
-    <h1>Login</h1>
+    <FormContainer>
+    <h1>Sign In</h1>
+    <Form onSubmit={submitHandler}>
+      <Form.Group controlId="email">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId="password">
+        <Form.Label>Password </Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+
+      <Button type="submit" variant="primary">
+        Sign In
+      </Button>
+    </Form>
+
+  </FormContainer>
   );
 };
 
